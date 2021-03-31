bb859143d373af3cddc1863463a9bf1ca6ce8329
MD MUDASSIR ALI <mma10@iitbbs.ac.in>
2021-03-31 22:51:16 +0530

    Basic backend set

---
+++ backend/server.js
@@ -0,0 +1,33 @@
+// Import dependencies
+
+const express = require('express');
+const path =  require('path')
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+
+// Connect to MySQL
+
+const mysql = require('mysql');
+var con = mysql.createConnection({
+    host: "remotemysql.com",
+    user: "lJForg58Kr",
+    password: "GOgHlsKN1d",
+    database:"lJForg58Kr"
+});  
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+});
+
+// APIs
+
+app.get('/demo',(req,res) => {
+    res.json({
+        "data": "done"
+    });
+});
+
+const port = process.env.PORT || 4000;
+app.listen(port);
