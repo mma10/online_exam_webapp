6610317666a55cfbc3887c6d1102324f945ab5b5
MD MUDASSIR ALI <mma10@iitbbs.ac.in>
2021-04-12 22:16:01 +0530

    a commit

--- frontend/src/components/students/student.js
+++ frontend/src/components/students/student.js
@@ -4,13 +4,14 @@ import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import propTypes from 'prop-types';
 
+import { showExamPaper } from '../../store/actions/studentActions';
+
 class student extends Component{
     static propTypes = {
         exams: propTypes.array.isRequired
     }
 
     render(){
-
 
         return(
             <div className = "">
